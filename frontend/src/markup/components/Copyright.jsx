cb0f4fe4f79de989dad86b49a75d770bbe22e5a4
Ephraim Debel <ephraimdebel@gmail.com>
2025-08-10 22:09:39 +0300

    copy right file added

---
+++ frontend/src/markup/components/Copyright.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Copyright = () => {
+  return (
+    <div className="clv_copyright_wrapper">
+		<p>copyright &copy; 2025 <a href="https://tech.pentatonics.studio" target='_blank'>pentatonics Technology</a> all right reserved.</p>
+	</div>
+  )
+}
+
+export default Copyright
